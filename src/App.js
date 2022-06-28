090822eed2524e1508106ec3b124b73f719453bd
Tohir <guvalakat1603@gmail.com>
2022-06-28 22:50:36 +0500

    add pages use react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
 
 function App() {
   return (
-    <div className="App">
-      Start
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotel" element={<List />} />
+        <Route path="/hotel/:id" element={<Hotel />} />
+      </Routes>
+    </Router>
   );
 }
 
