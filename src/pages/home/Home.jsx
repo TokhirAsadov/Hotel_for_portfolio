c2d82a2337295097c70c7018d8f74ef843ea6617
Tohir <guvalakat1603@gmail.com>
2022-06-28 22:51:10 +0500

    create Home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import "./home.css"
+import NavBar from "../../components/navbar/NavBar";
+import Header from "../../components/header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
