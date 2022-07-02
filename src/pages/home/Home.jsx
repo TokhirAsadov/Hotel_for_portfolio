d356449721607efa1fcce9fe30d7e16554ae7dbe
Tohir <guvalakat1603@gmail.com>
2022-07-02 21:44:38 +0500

    add component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import "./home.css"
 import NavBar from "../../components/navbar/NavBar";
 import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
   return (
@@ -8,7 +9,7 @@ const Home = () => {
       <NavBar />
       <Header />
       <div className="homeContainer">
-
+        <Featured />
       </div>
     </div>
   );
