6d32de2a79a708c5f9ac6f962868f2f1ea70b648
Tohir <guvalakat1603@gmail.com>
2022-07-02 22:55:40 +0500

    add properties

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import "./home.css"
 import NavBar from "../../components/navbar/NavBar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
 
 const Home = () => {
   return (
@@ -10,6 +11,9 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
       </div>
     </div>
   );
